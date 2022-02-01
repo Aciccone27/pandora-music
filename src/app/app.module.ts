70f041f36828bcee3548dae51666424196c24e70
ACiccone27 <ajfarson@mix.wvu.edu>
2022-02-01 15:39:19 -0500

    Finished up the bottom navigation for playing music

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { TopNavBarComponent } from 'src/app/navigation/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent } from './bottom_Navigation/bottomnavbar.component';
+import { CardComponent } from './cards/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    CardComponent,
+    BottomNavBarComponent
   ],
   imports: [
     BrowserModule
