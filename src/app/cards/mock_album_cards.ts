c2185a909d2eac3d41553e88193302c2c7069e38
ACiccone27 <ajfarson@mix.wvu.edu>
2022-02-07 23:46:02 -0500

    Created mock list for component

---
+++ src/app/cards/mock_album_cards.ts
@@ -0,0 +1,29 @@
+export const mock_album_cards = [
+    {
+    img: "./assets/encanto.jpg",
+    title: "Encanto (Original Motion Picture Soundtrack)",
+    artist:"Lin-Manuel Miranda, Germaine Franco & Encanto - Cast",
+    songs: 44
+    },
+
+    {
+        img: "./assets/meatloaf_thumbnail.jpg",
+        title: "Bat Out Of Hell",
+        artist: "Meatloaf",
+        songs: 6
+    },
+
+    {
+        img: "./assets/momma_mia_thumbnail.jpg",
+        title: "Mamma Mia! Here We Go Again (Original Motion Picture Soundtrack)",
+        artist: "Cast Of Mamma Mia! Here We Go Again",
+        songs: 18
+    },
+    
+    {
+        img: "./assets/shum_goa_thumbnail.jpg",
+        title: "SHUM",
+        artist: "Go-A",
+        songs: 1
+    }
+];
